ddb089b58995a1bef4a12eb2872a3db18f66507d
baykalov-maxim <baykalov.maxim@gmail.com>
2022-08-20 19:13:19 +0200

    init jest

---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export const testfunc = () => 3
---
+++ test/a.test.ts
@@ -0,0 +1,7 @@
+import { testfunc } from '../src/index';
+
+describe('test', () => {
+  it('test', () => {
+    expect(testfunc()).toBe(3);
+  })
+})
